92b933922b61764989691ce1d74d4a7421007cbe
frannpineroo <frannpinero@hotmail.com>
2025-08-08 17:46:36 -0300

    aniado entidad Usuario

--- entidades/models.py
+++ entidades/models.py
@@ -7,6 +7,16 @@ class Exercise(models.Model):
     equipment = models.CharField(max_length=100, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+class Usuario(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    dni = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    rol = models.CharField(max_length=50)
+    active = models.BooleanField(default=True)
+
     def __str__(self):
         return self.name
 
