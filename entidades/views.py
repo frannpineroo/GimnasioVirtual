91937bdfb3b75577f63d7ab8a2160eb79fe9a4af
BrunoSponton <brunosponton1@gmail.com>
2025-11-16 13:26:56 -0300

    adicion de rutas de archivos en views.py

--- entidades/views.py
+++ entidades/views.py
@@ -11,3 +11,21 @@ def exercise_page(request):
 
 def new_exercise(request):
     return render(request, 'nuevo-ejercicio.html')
+
+def client_page(request):
+    return render(request, 'clientes.html')
+
+def new_client(request):
+    return render(request, 'nuevo-cliente.html')
+
+def routine_page(request):
+    return render(request, 'rutinas.html')
+
+def new_routine(request):
+    return render(request, 'nueva-rutina.html')
+
+def equipment_page(request):
+    return render(request, 'equipos.html')
+
+def new_equipment(request):
+    return render(request, 'nuevo-equipo.html')
